91076223ee22457fbfe80280b91cb16e7b80301d
daire <dairecoleman@gmail.com>
2025-09-17 12:16:26 -0400

    adding node_exporter, script to automate local deployment

---
+++ scripts/run_grafantasy.sh
@@ -0,0 +1,35 @@
+# script to automate starting a local instance of grafantasy for quick testing/running
+# Phase 1
+# start node_exporter, prometheus (docker), grafana (docker)
+#
+# Author: Daire Coleman
+# Created: 17/11/2025
+
+# Resolve the absolute path of this script
+SCRIPT_PATH="$(readlink -f "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+
+echo "Script full path: $SCRIPT_PATH"
+echo "Script directory: $SCRIPT_DIR"
+
+# node_exporter
+
+# start cmd
+"$SCRIPT_DIR"/../modules/node_exporter/node_exporter > /dev/null 2>&1 &
+echo "verifying step"
+# verify its up before moving on
+success=0
+for i in $(seq 1 10); do
+    if curl -s -o /dev/null http://localhost:9100/metrics ; then
+        echo "✅ node_exporter is up (attempt $i)"
+        success=1
+        break
+    fi
+    echo "Waiting for node_exporter... ($i/10)"
+    sleep 1
+done
+
+if [ $success -eq 0 ]; then
+    echo "❌ node_exporter did not start after 10 seconds"
+    exit 1
+fi
